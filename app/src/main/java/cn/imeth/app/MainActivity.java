adfa20613531d8d6abbed6c33ed71a3eb7646057
ADi <daiwenlong@imeth.cn>
2015-06-19 12:01:50 +0800

    添加arcmenu控件

--- app/src/main/java/cn/imeth/app/MainActivity.java
+++ app/src/main/java/cn/imeth/app/MainActivity.java
@@ -5,7 +5,12 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.capricorn.ArcMenu;
+import com.capricorn.RayMenu;
 
 import cn.imeth.android.activity.ImethActivity;
 import cn.imeth.android.utils.Androids;
@@ -15,6 +20,14 @@ import cn.imeth.app.image.slider.ImageSliderActivity;
 
 
 public class MainActivity extends ImethActivity {
+
+    private static final int[] ITEM_DRAWABLES = {
+            R.drawable.composer_camera,
+            R.drawable.composer_music,
+            R.drawable.composer_place,
+            R.drawable.composer_sleep,
+            R.drawable.composer_thought,
+            R.drawable.composer_with };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,6 +78,28 @@ public class MainActivity extends ImethActivity {
             }
         });
 
+        ArcMenu arcMenu = (ArcMenu) findViewById(R.id.arc_menu);
+        ArcMenu arcMenu2 = (ArcMenu) findViewById(R.id.arc_menu_2);
+
+        initArcMenu(arcMenu, ITEM_DRAWABLES);
+        initArcMenu(arcMenu2, ITEM_DRAWABLES);
+
+        RayMenu rayMenu = (RayMenu) findViewById(R.id.ray_menu);
+        final int itemCount = ITEM_DRAWABLES.length;
+        for (int i = 0; i < itemCount; i++) {
+            ImageView item = new ImageView(this);
+            item.setImageResource(ITEM_DRAWABLES[i]);
+
+            final int position = i;
+            rayMenu.addItem(item, new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(MainActivity.this, "position:" + position, Toast.LENGTH_SHORT).show();
+                }
+            });// Add a menu item
+        }
+
     }
 
     @Override
@@ -87,6 +122,23 @@ public class MainActivity extends ImethActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void initArcMenu(ArcMenu menu, int[] itemDrawables) {
+        final int itemCount = itemDrawables.length;
+        for (int i = 0; i < itemCount; i++) {
+            ImageView item = new ImageView(this);
+            item.setImageResource(itemDrawables[i]);
+
+            final int position = i;
+            menu.addItem(item, new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(MainActivity.this, "position:" + position, Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
     }
 
 }
